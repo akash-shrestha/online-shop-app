31d76ab51472f2aec021a300d0ce7164c6869285
akash shrestha <akash_shrestha01@hotmail.com>
2021-05-03 12:53:04 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const bodyParser = require('body-parser');                                //for urlencoded form data
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);         //for storing session on DB
+const csrf = require('csurf');
+const flash = require('connect-flash');                                   //for attatching message on every incoming new req for showing error message
+const multer = require('multer');                                         //for multi-part form data
+
+const path = require('path');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+
+const MONGODB_URI =
+  '*mongodb uri here*';
+
+
+const app = express();
+
+const store = new MongoDBStore({           //for storing session on DB
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+
+const csrfProtection = csrf();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now().toString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');                                        //not needed if the default view folder ie 'views' is used
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(
+  session({
+    secret: 'my secret string',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+
+app.use(csrfProtection);
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      next(new Error(err));
+    });
+});
+
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+app.get('/500', errorController.get500);
+app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
+  console.log(error);
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
+    console.log('Connected to the database server !');
+    app.listen(3000);
+    console.log('Server is listening for incoming requests at port 3000 !');
+  })
+  .catch(err => {
+    console.log(err);
+  });
